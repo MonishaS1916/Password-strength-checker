8e6381d361a9c2d1c5fe0dd104c58eeb5bba4ebd
MonishaS1916 <monishasrinivasan1983@gmail.com>
2026-07-04 14:46:44 +0530

    Add files via upload

---
+++ password_checker.py
@@ -0,0 +1,27 @@
+password = input("Enter your password: ")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if any(char.isupper() for char in password):
+    score += 1
+
+if any(char.islower() for char in password):
+    score += 1
+
+if any(char.isdigit() for char in password):
+    score += 1
+
+special = "!@#$%^&*()-_=+[]{};:'\",.<>?/\\|"
+
+if any(char in special for char in password):
+    score += 1
+
+if score <= 2:
+    print("Weak Password")
+elif score <= 4:
+    print("Medium Password")
+else:
+    print("Strong Password")
